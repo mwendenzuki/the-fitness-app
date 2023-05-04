462837511895f5e60045b3d2a1880441c78a8c28
Mwende Nzuki <nzuki1339@gmail.com>
2023-05-04 13:40:17 +0300

    "first commit"

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { Box } from '@mui/material'
+
+import HeroBanner from '../components/HeroBanner'
+import SearchExercises from '../components/SearchExercises'
+//import Exercises from '../components/Exercises'
+
+const Home = () => {
+  //const [ exercises, setExercises ] = useState("all")
+  const [ bodyPart, setBodyPart ] = useState([])
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises 
+        //setExercises={setExercises} 
+        bodyPart={bodyPart} 
+        setBodyPart={setBodyPart}
+      />
+      {/* <Exercises 
+        setExercises={setExercises} 
+        bodyPart={bodyPart} 
+        exercises={exercises} 
+     /> */}
+    </Box>
+  )
+}
+
+export default Home
